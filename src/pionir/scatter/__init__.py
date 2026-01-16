5c753a58193bb7ae06512224d56fbfd346c29920
Lukas Schulig <schuligl@uni-greifswald.de>
2026-01-16 12:46:36 +0100

    Add `rnv` and `RNVTransformer` to scatter transformations and update exports

--- src/pionir/scatter/__init__.py
+++ src/pionir/scatter/__init__.py
@@ -1,3 +1,9 @@
+from .msc import msc
+from .rnv import rnv
 from .snv import snv
 
-__all__ = ['snv']
+__all__ = [
+    "msc",
+    "rnv",
+    "snv"
+]
--- src/pionir/sklearn/transformer.py
+++ src/pionir/sklearn/transformer.py
@@ -1,4 +1,9 @@
 from ..scatter.msc import MSCTransformer
+from ..scatter.rnv import RNVTransformer
 from ..scatter.snv import SNVTransformer
 
-__all__ = ["MSCTransformer", "SNVTransformer"]
+__all__ = [
+    "MSCTransformer",
+    "RNVTransformer",
+    "SNVTransformer"
+]
