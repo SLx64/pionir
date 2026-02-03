50d8e4565b8fec6dc7d4ab73576c4325a7a60567
Lukas Schulig <schuligl@uni-greifswald.de>
2026-02-03 14:54:02 +0100

    Add `Metadata` to core exports in `__init__.py`

--- src/pionir/core/__init__.py
+++ src/pionir/core/__init__.py
@@ -1,4 +1,5 @@
 from .collection import SpectrumCollection
+from .metadata import Metadata
 from .spectrum import Spectrum
 
-__all__ = ["SpectrumCollection", "Spectrum"]
+__all__ = ["SpectrumCollection", "Spectrum", "Metadata"]
